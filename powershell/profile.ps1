c68bcacc7feb8571f9368bf38e9d27aa97778f19
seakuya <seakuya@foxmail.com>
2022-06-27 23:08:34 +0800

    added color customed in setting for vscode

--- powershell/profile.ps1
+++
@@ -1,6 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-(& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-#endregion
-
