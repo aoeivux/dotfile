ebeddb977c7123ef0ade4ce556c6e0737477df2e
Yongxing Zhu <1617003785@qq.com>
2024-02-01 20:09:28 +0800

    Create new_profile.ps1

---
+++ powershell/new_profile.ps1
@@ -0,0 +1,40 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+#Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias ra lf
+Set-Alias g git
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias lg lazygit
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+
+
+# proxy_on.ps1
+
+function Set-ProxyOn {
+    $env:HTTP_PROXY = "http://127.0.0.1:7890"
+    $env:HTTPS_PROXY = "http://127.0.0.1:7890"
+    $env:ALL_PROXY = "socks5://127.0.0.1:7890"
+    Write-Host "Proxy set to http://127.0.0.1:7890 for HTTP and HTTPS, and socks5://127.0.0.1:7890 for all."
+}
+
+Set-Alias proxy_on Set-ProxyOn
+
+# proxy_off.ps1
+
+function Set-ProxyOff {
+    [System.Environment]::SetEnvironmentVariable("HTTP_PROXY", $null, [System.EnvironmentVariableTarget]::Process)
+    [System.Environment]::SetEnvironmentVariable("HTTPS_PROXY", $null, [System.EnvironmentVariableTarget]::Process)
+    [System.Environment]::SetEnvironmentVariable("ALL_PROXY", $null, [System.EnvironmentVariableTarget]::Process)
+    Write-Host "Proxy settings removed."
+}
+
+Set-Alias proxy_off Set-ProxyOff
